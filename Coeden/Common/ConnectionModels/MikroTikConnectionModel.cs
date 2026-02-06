70fa07ab36ff27da328d4c2099d29c761e4afd25
Cerys <cerysamberlewis@gmail.com>
2026-02-06 21:42:23 +0000

    added MikroTik

---
+++ Coeden/Common/ConnectionModels/MikroTikConnectionModel.cs
@@ -0,0 +1,13 @@
+﻿using Coeden.Common.Superclasses;
+using System.Text.Json.Serialization;
+
+namespace Coeden.Common.ConnectionModels
+{
+    internal class MikroTikConnectionModel : ConnectionModel
+    {
+        public override string Name => "MikroTik";
+
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
